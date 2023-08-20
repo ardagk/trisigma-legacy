b512fe4db7ce56cf0abf01c68a5c2c12f71fd45b
Arda Gök <ardagk@Ardas-MacBook-Air.local>
2023-08-20 00:23:11 -0500

    mq log handler added

---
+++ trisigma/infra/common/logger/rabbitmq_handler/__init__.py
@@ -0,0 +1,5 @@
+from .filters import FieldFilter  # noqa: F401
+from .formatters import JSONFormatter  # noqa: F401
+from .handlers import RabbitMQHandler  # noqa: F401
+
+__version__ = '2.2.0'
---
+++ trisigma/infra/common/logger/rabbitmq_handler/compat.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# Thanks to https://github.com/lobziik/rlog
+import sys
+import json  # noqa: F401
+from json.encoder import JSONEncoder as JSONEncoder  # noqa: F401
+ExceptionReporter = None
