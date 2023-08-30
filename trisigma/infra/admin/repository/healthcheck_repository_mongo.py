e389201e1a85d0d447728f1295c9c2be7b06bb03
Arda Gök <ardagk@Ardas-MacBook-Air.local>
2023-08-29 21:58:32 -0500

    mongo healthcheck description parameter added

--- trisigma/infra/admin/repository/healthcheck_repository_mongo.py
+++ trisigma/infra/admin/repository/healthcheck_repository_mongo.py
@@ -7,11 +7,11 @@ class HealthcheckRepositoryMongo:
         self._db = self._client['healthcheck']
         self._results_coll = self._db['results']
 
-    def push_result(self, time: float, title: str, value):
-        doc = {'time': time, 'title': title, 'value': value}
+    def push_result(self, time: float, title: str, value, desc=None):
+        doc = {'time': time, 'title': title, 'value': value, "desc": desc}
         self._results_coll.insert_one(doc)
 
-    def get_results(self, start_time: int, title: str):
-        query = {'time': {'$gte': start_time}, 'title': title}
+    def get_results(self, start_time: int, title: str, desc=None):
+        query = {'time': {'$gte': start_time}, 'title': title, 'desc': desc}
         cursor = self._results_coll.find(query, {'_id': 0})
         return list(cursor)
